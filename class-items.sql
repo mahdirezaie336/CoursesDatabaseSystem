d13f877a12e0e8366f821db378ce78d579bc2696
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-07-02 16:18:05 +0430

    Finish creating home work and questions

--- class-items.sql
+++ class-items.sql
@@ -10,9 +10,15 @@ create table if not exists Quiz (
     finished int default 0
 );
 
-create table if not exists QuadraticQuestion (
-    question_id int primary key auto_increment,
+# Questions' superclass
+create table if not exists Question (
+    question_id int primary key,
     question_body varchar(512),
+    answer_description varchar(512)
+);
+
+create table if not exists QuadraticQuestion (
+    question_id int primary key references Question,
     answer_1 varchar(512),
     answer_2 varchar(512),
     answer_3 varchar(512),
@@ -21,8 +27,28 @@ create table if not exists QuadraticQuestion (
     check ( correct_answer in (1, 2, 3, 4) )
 );
 
+create table if not exists ShortAnswerQuestion (
+    question_id int primary key references Question,
+    correct_answer varchar(512)
+);
+
+# Relation between question and quiz
 create table if not exists QuestionQuiz (
     quiz_id int references Quiz,
     question_id int references QuadraticQuestion,
     primary key (quiz_id, question_id)
-)
+);
+
+create table if not exists Homework (
+    homework_id int primary key,
+    homework_name varchar(512)
+);
+
+# Relation between homework and Short Answer Question
+create table if not exists HomeworkQuestion (
+    homework_id int references Homework,
+    question_id int references ShortAnswerQuestion,
+    primary key (homework_id, question_id)
+);
+
+
