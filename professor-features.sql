7b99cdc93b3bd5944b20bcd032973584078c41ef
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-07-02 18:26:07 +0430

    Create procedure create_homework

--- professor-features.sql
+++ professor-features.sql
@@ -55,8 +55,48 @@ begin
     end if;
 end;
 
+# Professor can create homework
+create procedure create_homework (in
+    token varchar(512),
+    homework_name varchar(512),
+    course_id char(8),
+    deadline datetime)
+begin
+    if check_professor_login(token) then
+        # Checking if the course is for the professor
+        if not exists(select *
+                      from Course C
+                      where C.professor_no = get_professor(token) and
+                            C.course_id = course_id) then
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Access Denied';
+        end if;
+
+        insert into Homework (homework_name, course_id, deadline)
+        values (homework_name, course_id, deadline);
+    else
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
+    end if;
+end;
+
+create procedure create_quiz (in
+    token varchar(512),
+
+    )
+
 select user_login('12001', '3795148131Aa');
 call view_class_members('43c5b1dfd5079b426167a2ef05e47c88');
 call view_class_quizzes('43c5b1dfd5079b426167a2ef05e47c88');
 call view_class_homeworks('43c5b1dfd5079b426167a2ef05e47c88');
 call view_class_homeworks('a');
+call create_homework(
+    '43c5b1dfd5079b426167a2ef05e47c88',
+    'HW6',
+    '17000002',
+    '2022-07-09 23:59:59'
+    );
+call create_homework(
+    '43c5b1dfd5079b426167a2ef05e47c88',
+    'HW6',
+    '12000004',
+    '2022-07-09 23:59:59'
+    );
