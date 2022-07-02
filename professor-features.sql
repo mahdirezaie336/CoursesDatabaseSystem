d198622100dbea852a874474dff1d406cd621057
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-07-02 15:50:28 +0430

    Fix get_professor bug and change columns in view_class_members

--- professor-features.sql
+++ professor-features.sql
@@ -7,17 +7,18 @@ begin
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
     end if;
 
-    return (select professor_no from ProfessorLogins PL where Pl.token=token);
+    return (select professor_no from ProfessorLogins PL where PL.token=token);
 end;
 
 # Professor must can see class members
 create procedure view_class_members (in token varchar(512))
 begin
-    select *
+    select S.student_no, S.name_fa, S.email
     from Course C
         join Takes T on C.course_id = T.course_id
+        join Student S on S.student_no = T.student_no
     where C.professor_no = get_professor(token);
 end;
 
 select user_login('12001', '5593661a793cf7e9044311cfae51945b');
-call view_courses()
+call view_class_members('201088c547cfdb9855cd949d2bc80f67');
