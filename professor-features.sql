e275a1cedb82513108b01b1fa7a672893036f64c
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-07-02 17:13:19 +0430

    Create procedure for view quizzes and homeworks

--- professor-features.sql
+++ professor-features.sql
@@ -20,5 +20,25 @@ begin
     where C.professor_no = get_professor(token);
 end;
 
+# Professor can see list of quizzes
+create procedure view_class_quizzes (in token varchar(512))
+begin
+    select quiz_id, quiz_name, start_datetime, finish_datetime, duration, finish_datetime
+    from Course C
+        join Quiz Q on C.course_id = Q.course_id
+    where C.professor_no = get_professor(token);
+end;
+
+# Professor can see list of homeworks
+create procedure view_class_homeworks (in token varchar(512))
+begin
+    select homework_id, homework_name
+    from Course C
+        join Homework H on C.course_id = H.course_id
+    where professor_no = get_professor(token);
+end;
+
+
+
 select user_login('12001', '5593661a793cf7e9044311cfae51945b');
 call view_class_members('201088c547cfdb9855cd949d2bc80f67');
