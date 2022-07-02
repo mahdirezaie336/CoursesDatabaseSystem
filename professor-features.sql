610b5f8d55ac7842ad2df631992930c4e4f04d74
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-07-02 17:49:31 +0430

    Create check professor login login and use it in procedures

--- professor-features.sql
+++ professor-features.sql
@@ -1,9 +1,14 @@
 use EducationalSystem;
 
+create function check_professor_login (token varchar(512)) returns bool
+begin
+    return exists(select * from ProfessorLogins PL where PL.token=token);
+end;
+
 create function get_professor (token varchar(512)) returns varchar(512)
 begin
     # Check if token is valid
-    if not token in (select token from ProfessorLogins) then
+    if not check_professor_login(token) then
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
     end if;
 
@@ -13,34 +18,45 @@ end;
 # Professor must can see class members
 create procedure view_class_members (in token varchar(512))
 begin
-    select S.student_no, S.name_fa, S.email
-    from Course C
-        join Takes T on C.course_id = T.course_id
-        join Student S on S.student_no = T.student_no
-    where C.professor_no = get_professor(token);
+    if check_professor_login(token) then
+        select S.student_no, S.name_fa, S.email
+        from Course C
+            join Takes T on C.course_id = T.course_id
+            join Student S on S.student_no = T.student_no
+        where C.professor_no = get_professor(token);
+    else
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
+    end if;
 end;
 
 # Professor can see list of quizzes
 create procedure view_class_quizzes (in token varchar(512))
 begin
-    select quiz_id, quiz_name, start_datetime, finish_datetime, duration, finish_datetime
-    from Course C
-        join Quiz Q on C.course_id = Q.course_id
-    where C.professor_no = get_professor(token);
+    if check_professor_login(token) then
+        select quiz_id, quiz_name, start_datetime, finish_datetime, duration, finish_datetime
+        from Course C
+            join Quiz Q on C.course_id = Q.course_id
+        where C.professor_no = get_professor(token);
+    else
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
+    end if;
 end;
 
 # Professor can see list of homeworks
 create procedure view_class_homeworks (in token varchar(512))
 begin
-    select homework_id, homework_name
-    from Course C
-        join Homework H on C.course_id = H.course_id
-    where professor_no = get_professor(token);
+    if check_professor_login(token) then
+        select homework_id, homework_name
+        from Course C
+            join Homework H on C.course_id = H.course_id
+        where professor_no = get_professor(token);
+    else
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are not logged in';
+    end if;
 end;
-
-
 
 select user_login('12001', '3795148131Aa');
 call view_class_members('43c5b1dfd5079b426167a2ef05e47c88');
 call view_class_quizzes('43c5b1dfd5079b426167a2ef05e47c88');
 call view_class_homeworks('43c5b1dfd5079b426167a2ef05e47c88');
+call view_class_homeworks('a');
